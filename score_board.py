fc432a7579d73117af7b2ef1e7ba3ef98dbb4703
Arthur Martins <arthurcarbonari99@gmail.com>
2022-11-25 22:05:17 +0000

    Remove useless method, method calls and rename vars

--- score_board.py
+++ score_board.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QVBoxLayout, QWidget, QLabel
+from PyQt6.QtWidgets import QVBoxLayout, QLabel
 from PyQt6.QtCore import pyqtSlot
 
 
@@ -9,8 +9,8 @@ class ScoreBoard(QLabel):
         super().__init__()
 
         # create two labels which will be updated by signals
-        self.label_clickLocation = QLabel("Click Location: ")
-        self.label_timeRemaining = QLabel("Time remaining: ")
+        self.label_click_location = QLabel("Click Location: ")
+        self.label_time_remaining = QLabel("Time remaining: ")
 
         self.init_ui()
 
@@ -18,18 +18,9 @@ class ScoreBoard(QLabel):
         """initiates ScoreBoard UI"""
 
         # create a widget to hold other widgets
-        mainLayout = QVBoxLayout(self)
-        mainLayout.addWidget(self.label_clickLocation)
-        mainLayout.addWidget(self.label_timeRemaining)
-
-        self.resize(200, 200)
-        self.center()
-        self.setWindowTitle('ScoreBoard')
-
-        self.show()
-
-    def center(self):
-        """centers the window on the screen, you do not need to implement this method"""
+        main_layout = QVBoxLayout(self)
+        main_layout.addWidget(self.label_click_location)
+        main_layout.addWidget(self.label_time_remaining)
 
     def make_connection(self, board):
         """this handles a signal sent from the board class"""
@@ -42,7 +33,7 @@ class ScoreBoard(QLabel):
     def set_click_location(self, click_loc):
         """updates the label to show the click location"""
 
-        self.label_clickLocation.setText("Click Location:" + click_loc)
+        self.label_click_location.setText("Click Location:" + click_loc)
         print('slot ' + click_loc)
 
     @pyqtSlot(int)
@@ -50,6 +41,6 @@ class ScoreBoard(QLabel):
         """updates the time remaining label to show the time remaining"""
 
         update = "Time Remaining:" + str(time_remaining)
-        self.label_timeRemaining.setText(update)
+        self.label_time_remaining.setText(update)
         print('slot ' + update)
         # self.redraw()
