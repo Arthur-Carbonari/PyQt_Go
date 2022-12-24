38e19ff553fbbd2017c19121f8cf0145d15e9af0
Raven <mert.bekar@student.griffith.ie>
2022-12-24 20:11:39 +0000

    score board aligned

--- score_board.py
+++ score_board.py
@@ -1,21 +1,29 @@
 from PyQt6.QtGui import QPixmap, QPainter
-from PyQt6.QtWidgets import QVBoxLayout, QLabel, QWidget, QFrame
-from PyQt6.QtCore import pyqtSlot, QPoint
+from PyQt6.QtWidgets import QVBoxLayout, QLabel, QWidget, QFrame, QHBoxLayout, QPushButton
+from PyQt6.QtCore import pyqtSlot, QPoint, Qt
 
 
 # TODO names, turn counter, timer, skip button
 
 class ScoreBoard(QWidget):
     """ base the score_board on a QLabel"""
+
     def __init__(self):
         super().__init__()
-        self.background = QPixmap("./icons/sb_background.png")
+
+        self.captured_pieces = [0, 0]
 
         self.players = ["Black", "White"]
-        # create two labels which will be updated by signals
-        self.player_label = QLabel("Current Player: " + self.players[0])
-        self.label_click_location = QLabel("Click Location: ")
-        self.label_time_remaining = QLabel("Time remaining: ")
+        self.current_player = 0
+        # TODO: this labels will update with proper signals
+        self.player_label = QLabel(self.players[self.current_player])
+        self.black_captured = QLabel(str(self.captured_pieces[0]))
+        self.white_captured = QLabel(str(self.captured_pieces[1]))
+        self.time_label = QLabel("Time Remaining: NA")
+        # TODO: this buttons activate signals
+        self.undo_btn = QPushButton("Undo")
+        self.skip_btn = QPushButton("Skip")
+        self.redo_btn = QPushButton("Redo")
 
         self.init_ui()
 
@@ -24,9 +32,83 @@ class ScoreBoard(QWidget):
 
         # create a widget to hold other widgets
         main_layout = QVBoxLayout(self)
-        main_layout.addWidget(self.player_label)
-        main_layout.addWidget(self.label_click_location)
-        main_layout.addWidget(self.label_time_remaining)
+
+        # creating line layouts
+
+        # current player layout
+        current_player_layout = QVBoxLayout()
+
+        # current player header line
+        player_header_line = QHBoxLayout()
+        current_player_layout.addLayout(player_header_line)
+        # adding stretches to align QLabel
+        player_header_line.addStretch()
+        player_header_line.addWidget(QLabel("=== Current Player ==="))
+        player_header_line.addStretch()
+
+        # label line
+        player_label_line = QHBoxLayout()
+        current_player_layout.addLayout(player_label_line)
+        # adding stretches to align QLabel
+        player_label_line.addStretch()
+        player_label_line.addWidget(self.player_label)
+        player_label_line.addStretch()
+
+        # captured pieces layout
+        captured_pieces_layout = QVBoxLayout()
+
+        # captured pieces main header line
+        captured_main_header_line = QHBoxLayout()
+        captured_pieces_layout.addLayout(captured_main_header_line)
+        # adding stretches to align QLabel
+        captured_main_header_line.addStretch()
+        captured_main_header_line.addWidget(QLabel("=== Captured Pieces ==="))
+        captured_main_header_line.addStretch()
+
+        # layout for secondary headers and counters
+        pieces_layout = QVBoxLayout()
+        captured_pieces_layout.addLayout(pieces_layout)
+
+        # set captured pieces secondary headers line
+        pieces_header_line = QHBoxLayout()
+        pieces_layout.addLayout(pieces_header_line)
+        # adding stretches to align QLabels
+        pieces_header_line.addStretch()
+        pieces_header_line.addWidget(QLabel("Black"))
+        pieces_header_line.addStretch()
+        pieces_header_line.addWidget(QLabel("White"))
+        pieces_header_line.addStretch()
+
+        # set captured pieces amounts line
+        captured_amount_line = QHBoxLayout()
+        pieces_layout.addLayout(captured_amount_line)
+        # adding stretches to align QLabels
+        captured_amount_line.addStretch()
+        captured_amount_line.addWidget(self.black_captured)
+        captured_amount_line.addStretch()
+        captured_amount_line.addWidget(self.white_captured)
+        captured_amount_line.addStretch()
+
+        # timer line
+        timer_line = QHBoxLayout()
+        timer_line.addStretch()
+        timer_line.addWidget(self.time_label)
+        timer_line.addStretch()
+
+        # buttons line
+        buttons_line = QHBoxLayout()
+        buttons_line.addStretch()
+        buttons_line.addWidget(self.undo_btn)
+        buttons_line.addWidget(self.skip_btn)
+        buttons_line.addWidget(self.redo_btn)
+        buttons_line.addStretch()
+
+        # add all layouts to main layout
+        main_layout.addLayout(current_player_layout)
+        main_layout.addLayout(captured_pieces_layout)
+        main_layout.addLayout(timer_line)
+        main_layout.addLayout(buttons_line)
+
 
     def make_connection(self, board):
         """this handles a signal sent from the board class"""
@@ -41,24 +123,24 @@ class ScoreBoard(QWidget):
     def set_click_location(self, click_loc):
         """updates the label to show the click location"""
 
-        self.label_click_location.setText("Click Location:" + click_loc)
         print('slot ' + click_loc)
 
     @pyqtSlot(int)
     def set_time_remaining(self, time_remaining):
         """updates the time remaining label to show the time remaining"""
 
-        update = "Time Remaining:" + str(time_remaining)
-        self.label_time_remaining.setText(update)
+        update = "Time Remaining: " + str(time_remaining)
+        self.time_label.setText(update)
         # print('slot ' + update)
         # self.redraw()
 
     def change_player(self, player_no):
-        self.player_label.setText("Current Player: " + self.players[player_no])
+        self.current_player = player_no
+        self.player_label.setText(self.players[self.current_player])
 
     def game_over(self):
         """updates scoreboard to show scores and winner"""
-        self.label_time_remaining.setText("Game over, winner" + self.players[next(self.turn_counter)])
+        self.time_label.setText("Game over, winner " + self.players[self.current_player] + " player")
 
     # TODO: This could be prettier
     def paintEvent(self, event):
