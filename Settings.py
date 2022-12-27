f8e22e8da336ac3b1579ad11aad4804d84481f7e
Arthur Martins <arthurcarbonari99@gmail.com>
2022-12-27 05:59:59 +0000

    Create GameMode Enum that contains all available game modes

--- Settings.py
+++ Settings.py
@@ -1,5 +1,14 @@
+from enum import Enum
+
+
+class GameMode(Enum):
+    NORMAL = "Normal Go"
+    SPEED = "Speed Go"
+
 
 class Settings:
+
+    GAME_MODES = tuple(GameMode)
 
     TIMER_START = 10
     TIMER_SPEED = 1000
