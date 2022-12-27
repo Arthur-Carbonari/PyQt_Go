f3528d4fe7e8924d97cde9a50add5d2d36060d78
Arthur Martins <arthurcarbonari99@gmail.com>
2022-12-27 10:36:03 +0000

    Add Stylesheets constants to Settings

--- Settings.py
+++ Settings.py
@@ -15,3 +15,31 @@ class Settings:
 
     TIMER_START = 10
     TIMER_SPEED = 1000
+
+    # stylesheets ==================================
+
+    CURRENT_PLAYER_STYLESHEET = """
+        PlayerBox{
+            border: 2px solid #0000FF;
+            border-radius: 5px;
+            box-shadow: 0 0 10px #0000FF;
+        }
+        """
+
+    SCORE_BOARD_STYLESHEET = """
+        QGroupBox{
+            background-color: rgba(0, 0, 0, 0.7);
+        }
+        QLabel{
+            background-color: rgba(0, 0, 0, 0);
+            color: white;
+        }
+        QGroupBox#first{
+            border: 2px solid #0000FF;
+            border-radius: 5px;
+            box-shadow: 0 0 10px #0000FF;
+        }
+        PlayerBox{
+            background-color: rgba(200, 200, 200, 0.1);
+        }
+        """
